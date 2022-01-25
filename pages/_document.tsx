a06a35353445e7eb02e9357d9acdc989c057c28f
Hoang Van Thanh <thanhthanh125521@gmail.com>
2022-01-25 11:33:42 +0700

    a

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core";
+// import {ServerStyleSheets} from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
   render() {
